19f0f8be43beb2173c7722d0ad3a73f93bcb982c
Damian Wilding <wildman700@gmail.com>
2022-10-05 12:23:47 -0600

    added some classes

---
+++ Game/Casting/Trebuchet.cs
@@ -0,0 +1,8 @@
+namespace Trebuchet.Game.Casting;
+
+/// The trebuchet is the object that will be launching the ball
+
+public class Trebuchet : Actor
+{
+    
+}
---
+++ Game/Scripting/CheckCollision.cs
@@ -0,0 +1,19 @@
+using Trebuchet.Game.Casting;
+
+/// CheckCollision will check to see if the ball is touching the castle
+
+namespace Trebuchet.Game.Scripting 
+{
+    public class CheckCollision : Action
+    {
+
+
+        public void Execute(Cast cast, Script script)
+        {
+
+        }
+
+
+
+    }
+}
